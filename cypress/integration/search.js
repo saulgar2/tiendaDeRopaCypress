7c5d761d8faa71318e3ad5f9268cce78a1e32420
saulgar2 <saulgar2@gmail.com>
2021-05-19 16:49:19 -0500

    Actualizacion

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -5,19 +5,13 @@ context('Search elements', () => {
     })
 
     it('Search for elements with multiple results', () => {
-        cy.fixture('index').then((index) =>{
-            cy.get(index.searchBox).type('dress');
-            cy.get(index.searchButton).click();
-        })
+        cy.search('dress')
         cy.fixture('searchResult').then((searchResult) => {
             cy.get(searchResult.title).should('contain','dress');
         })
     })
     it('Search for elements with no results', () => {
-        cy.fixture('index').then((index) =>{
-            cy.get(index.searchBox).type('qwerty');
-            cy.get(index.searchButton).click();
-        })
+        cy.search('qwerty')
         cy.fixture('searchResult').then((searchResult) => {
             cy.get(searchResult.alert).should('contain','No results were found for your search');
         })
